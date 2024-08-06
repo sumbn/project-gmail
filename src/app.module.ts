d70bc3afa4d034855a1a7bd4fe3d1385a39df7bc
sum bùi <sumbn37k02.1@gmail.com>
2024-08-06 14:47:57 +0700

    chang module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from '../db/data-source';
+// import { dataSourceOptions } from '../db/data-source';
+import { Account } from './account/entities/account.entity';
+
+// @Module({
+//   imports: [TypeOrmModule.forRoot(dataSourceOptions), AccountModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), AccountModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-graphql-graphql-gmail.e.aivencloud.com',
+      port: 10177,
+      username: 'avnadmin',
+      password: 'AVNS_Pq88DUcTUIgqlkmWz39',
+      database: 'defaultdb',
+      entities: [Account],
+      synchronize: true,
+    }),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
