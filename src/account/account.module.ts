f34e19da48343ed4cfc3e83b6df5062e9a56625b
sum bùi <sumbn37k02.1@gmail.com>
2024-08-06 14:05:58 +0700

    change path

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Account } from './entities/account.entity';
+import { Account } from 'src/account/entities/account.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
