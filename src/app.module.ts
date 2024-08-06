c18b60f1304600a3e54ca1380c9a4905e4e39ece
sum bùi <sumbn37k02.1@gmail.com>
2024-08-06 15:19:25 +0700

    add env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AccountModule } from './account/account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // import { dataSourceOptions } from '../db/data-source';
 import { Account } from './account/entities/account.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 // @Module({
 //   imports: [TypeOrmModule.forRoot(dataSourceOptions), AccountModule],
@@ -14,15 +15,20 @@ import { Account } from './account/entities/account.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql-graphql-graphql-gmail.e.aivencloud.com',
-      port: 10177,
-      username: 'avnadmin',
-      password: 'AVNS_Pq88DUcTUIgqlkmWz39',
-      database: 'defaultdb',
-      entities: [Account],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE_NAME'),
+        entities: [Account],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     AccountModule,
   ],
