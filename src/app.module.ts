c830a953461c0b4a705b27411e669b733a7cc970
sum bùi <sumbn37k02.1@gmail.com>
2024-08-06 20:16:31 +0700

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,36 +3,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { dataSourceOptions } from '../db/data-source';
-import { Account } from './account/entities/account.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-// @Module({
-//   imports: [TypeOrmModule.forRoot(dataSourceOptions), AccountModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-
+import { dataSourceOptions } from '../db/data-source';
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE_NAME'),
-        entities: [Account],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    AccountModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
+//------------------OPTION 2----------------------------
+
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Account } from './account/entities/account.entity';
+// import { AccountModule } from './account/account.module';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot(),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: (configService: ConfigService) => ({
+//         type: 'mysql',
+//         host: configService.get('DB_HOST'),
+//         port: +configService.get('DB_PORT'),
+//         username: configService.get('DB_USERNAME'),
+//         password: configService.get('DB_PASSWORD'),
+//         database: configService.get('DB_DATABASE_NAME'),
+//         entities: [Account],
+//         synchronize: true,
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     AccountModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
 export class AppModule {}
