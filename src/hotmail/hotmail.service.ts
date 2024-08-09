84078a7d3825e0c7d284caaabfd953c363f6774a
sum bùi <sumbn37k02.1@gmail.com>
2024-08-09 22:04:39 +0700

    get otp

--- src/hotmail/hotmail.service.ts
+++ src/hotmail/hotmail.service.ts
@@ -18,9 +18,29 @@ export class HotmailService {
   async checkMail() {
     try {
       const emails = await getEmails();
-      return emails;
+      if (emails.length > 0) {
+        const lastEmail = emails[emails.length - 1];
+        const otp = this.extractOtp(lastEmail);
+        if (otp) {
+          // console.log('Extracted OTP:', otp);
+          return otp;
+        } else {
+          // console.log('No OTP found in the email.');
+          return null;
+        }
+      } else {
+        // console.log('No emails found.');
+        return null;
+      }
     } catch (error) {
-      console.error('error');
+      // console.error('Error fetching emails:', error);
+      throw new Error('Unable to fetch emails');
     }
+  }
+
+  private extractOtp(emailContent: string): string | null {
+    const otpRegex = /\b\d{6}\b/; // Biểu thức chính quy để tìm chuỗi 6 chữ số
+    const match = emailContent.match(otpRegex);
+    return match ? match[0] : null; // Trả về mã OTP nếu tìm thấy, ngược lại trả về null
   }
 }
