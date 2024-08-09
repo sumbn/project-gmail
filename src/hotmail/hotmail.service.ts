8b6988d8f47dc620a3e753abb33f0300450134a4
sum bùi <sumbn37k02.1@gmail.com>
2024-08-09 22:46:08 +0700

    format code

--- src/hotmail/hotmail.service.ts
+++ src/hotmail/hotmail.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HotMail } from './entities/hotmail.entity';
 import { Repository } from 'typeorm';
